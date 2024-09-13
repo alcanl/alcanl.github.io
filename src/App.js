db950194f3605e9aec75ad5d12cbf25c7f975c93
alcanl <alican.kecici@gmail.com>
2024-09-13 13:20:14 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-
-function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="*" element={<TokenPage />} />
-            </Routes>
-        </Router>
-    );
-}
-
-function HomePage() {
-    return <div>Welcome to the Home Page</div>;
-}
-
 function TokenPage() {
-    const location = useLocation();
-    const [errorMessage, setErrorMessage] = useState('');
+  const location = useLocation();
+  const [errorMessage, setErrorMessage] = useState('');
 
-    useEffect(() => {
-        const queryParams = new URLSearchParams(location.search);
-        const eMail = queryParams.get('eMail');
-        const token = queryParams.get('token');
+  useEffect(() => {
+      const queryParams = new URLSearchParams(location.search);
+      const eMail = queryParams.get('eMail');
+      const token = queryParams.get('token');
 
-        if (token && eMail) {
-            window.location.href = `http://localhost:50531/api/ear-technic/auth/reset-password?eMail=${eMail}&token=${token}`;
-        } else {
-            setErrorMessage('Token or eMail not found in the URL.');
-        }
-    }, [location.search]);
+      if (token && eMail) {
+          const redirectUrl = `http://localhost:50531/api/ear-technic/auth/reset-password?eMail=${eMail}&token=${token}`;
+          console.log('Redirecting to:', redirectUrl);
+          window.location.href = redirectUrl;
+      } else {
+          setErrorMessage('Token or eMail not found in the URL.');
+      }
+  }, [location.search]);
 
-    return (
-        <div>
-            <p id="error-message">{errorMessage}</p>
-        </div>
-    );
+  return (
+      <div>
+          <p id="error-message">{errorMessage}</p>
+      </div>
+  );
 }
-
-export default App;
