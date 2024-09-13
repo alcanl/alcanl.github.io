1438a448dca3ac8e1c9b07ecd9950f7dcae94630
alcanl <alican.kecici@gmail.com>
2024-09-13 09:32:15 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { HashRouter as Router, Route, Routes, useParams, useNavigate } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/:token" element={<TokenPage />} />
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+function HomePage() {
+    return <div>Welcome to the Home Page</div>;
+}
+
+function TokenPage() {
+    const { token } = useParams();  // URL'den token'ı al
+    const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState('');
+
+    useEffect(() => {
+        if (token) {
+            // Token varsa API'ye yönlendirme yap
+            window.location.href = `http://localhost:50531/api/ear-technic/auth/reset-password/${token}`;
+        } else {
+            // Token yoksa hata mesajı göster
+            setErrorMessage('Token not found in the URL path.');
+        }
+    }, [token]);
+
+    return (
+        <div>
+            <p id="error-message">{errorMessage}</p>
+        </div>
+    );
 }
 
 export default App;
