7f28f4a68733144451e1037659e104cc70e00881
alcanl <alican.kecici@gmail.com>
2024-09-13 13:27:29 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-function TokenPage() {
-  const location = useLocation();
-  const [errorMessage, setErrorMessage] = useState('');
+import React, { useEffect, useState } from 'react';
+import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
-  useEffect(() => {
-      const queryParams = new URLSearchParams(location.search);
-      const eMail = queryParams.get('eMail');
-      const token = queryParams.get('token');
-
-      if (token && eMail) {
-          const redirectUrl = `http://localhost:50531/api/ear-technic/auth/reset-password?eMail=${eMail}&token=${token}`;
-          console.log('Redirecting to:', redirectUrl);
-          window.location.href = redirectUrl;
-      } else {
-          setErrorMessage('Token or eMail not found in the URL.');
-      }
-  }, [location.search]);
-
-  return (
-      <div>
-          <p id="error-message">{errorMessage}</p>
-      </div>
-  );
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="*" element={<TokenPage />} />
+            </Routes>
+        </Router>
+    );
 }
+
+function HomePage() {
+    return <div>TEST DIRECTION</div>;
+}
+
+function TokenPage() {
+    const location = useLocation();
+    const [errorMessage, setErrorMessage] = useState('');
+
+    useEffect(() => {
+        const queryParams = new URLSearchParams(location.search);
+        const eMail = queryParams.get('eMail');
+        const token = queryParams.get('token');
+
+        if (token && eMail) {
+            window.location.href = `http://localhost:50531/api/ear-technic/auth/reset-password?eMail=${eMail}&token=${token}`;
+        } else {
+            setErrorMessage('Token or eMail not found in the URL.');
+        }
+    }, [location.search]);
+
+    return (
+        <div>
+            <p id="error-message">{errorMessage}</p>
+        </div>
+    );
+}
+
 export default App;
