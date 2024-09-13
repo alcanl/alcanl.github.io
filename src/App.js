2ac04db3505186088a44c5e1d9c8f95dc5d8a153
alcanl <alican.kecici@gmail.com>
2024-09-13 14:08:21 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
+
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="*" element={<TokenPage />} />
-            </Routes>
-        </Router>
-    );
+  console.log("App component loaded");
+  return (
+      <Router>
+          <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="*" element={<TokenPage />} />
+          </Routes>
+      </Router>
+  );
 }
 
 function HomePage() {
-    return <div>Welcome to the Home Page</div>;
+    return <div>TEST DIRECTION</div>;
 }
 
 function TokenPage() {
