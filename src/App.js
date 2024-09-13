65d93d6d343197efc3c5bb12833969c083a302c4
alcanl <alican.kecici@gmail.com>
2024-09-13 13:45:53 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 function App() {
     return (
@@ -13,7 +13,7 @@ function App() {
 }
 
 function HomePage() {
-    return <div>TEST DIRECTION</div>;
+    return <div>Welcome to the Home Page</div>;
 }
 
 function TokenPage() {
@@ -21,13 +21,20 @@ function TokenPage() {
     const [errorMessage, setErrorMessage] = useState('');
 
     useEffect(() => {
+        console.log("Current location:", location);
         const queryParams = new URLSearchParams(location.search);
         const eMail = queryParams.get('eMail');
         const token = queryParams.get('token');
 
+        console.log("eMail:", eMail);
+        console.log("Token:", token);
+
         if (token && eMail) {
+            // Yönlendirme yapıyoruz
+            console.log("test")
             window.location.href = `http://localhost:50531/api/ear-technic/auth/reset-password?eMail=${eMail}&token=${token}`;
         } else {
+            // Token veya eMail bulunamazsa hata mesajı gösteriyoruz
             setErrorMessage('Token or eMail not found in the URL.');
         }
     }, [location.search]);
