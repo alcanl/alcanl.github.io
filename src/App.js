6e6ee5fcae81c737fdeb297808cddfec5ab275b0
alcanl <alican.kecici@gmail.com>
2024-09-18 14:39:36 +0300

    added api key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import config from './config';
 
 function App() {
   console.log("App component loaded");
@@ -13,33 +14,50 @@ function App() {
 }
 
 function HomePage() {
-    TokenPage()
-    return <div>Redirecting...</div>;
+    return <TokenPage />;
 }
 
 function TokenPage() {
-  console.log("TEST");
   const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
+    console.log("useEffect triggered");
+    const hash = window.location.hash;
+    console.log("Hash:", hash);
+    const token = hash.substring(2);  
+    console.log("Extracted token:", token);
 
-      console.log("useEffect triggered");
-      const hash = window.location.hash;
-      console.log("Hash:", hash);
-      const token = hash.substring(2); 
-      console.log("Extracted token:", token);
+    if (token) {
+      fetch(`${config.apiUrl}?token=${token}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json', 
+          'apiAuthKey': config.apiKey 
+        }
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log("API response:", data);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        setErrorMessage('404 not found');
+      });
 
-      if (token) {
-          window.location.href = `http://localhost:50531/api/ear-technic/auth/reset-password?token=${token}`;
-      } else {
-          setErrorMessage('404 not found');
-      }
+    } else {
+      setErrorMessage('404 not found');
+    }
   }, []);
 
   return (
-      <div>
-          <p id="error-message">{errorMessage}</p>
-      </div>
+    <div>
+      <p id="error-message">{errorMessage}</p>
+    </div>
   );
 }
 
